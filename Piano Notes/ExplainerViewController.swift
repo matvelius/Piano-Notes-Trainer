3e398314e892d42c99d97d018b60b31c300bce2a
Matvey Kostukovsky <matvelius@gmail.com>
2019-03-19 20:59:13 -0500

    update explainers and add simple explainer navigation functionality

---
+++ Piano Notes/ExplainerViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ExplainerViewController.swift
+//  Piano Notes
+//
+//  Created by Matvey on 3/19/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class ExplainerViewController: UIViewController {
+    
+    var explainerIndex = 1
+
+    @IBOutlet weak var explainerImage: UIImageView!
+    
+    @IBOutlet weak var navTriangleLeft: UIButton!
+    
+    @IBOutlet weak var navTriangleRight: UIButton!
+    
+    @IBAction func navTriangleLeftPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func navTriangleRightPressed(_ sender: UIButton) {
+        explainerIndex += 1
+        explainerImage.image = UIImage(named: "explainer_\(explainerIndex)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        explainerImage.image = UIImage(named: "explainer_1")
+        
+//        imageName = "\(currentUserAnswer)_wrong"
+//        let image = UIImage(named: imageName)
+//        currentNoteButton.setImage(image, for: UIControl.State.normal)
+        
+        navTriangleLeft.isEnabled = false
+        
+        navTriangleLeft.setImage(UIImage(named: "nav_triangle_L_inactive"), for: UIControl.State.disabled)
+        
+        navTriangleRight.setImage(UIImage(named: "nav_triangle_R_normal"), for: UIControl.State.normal)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
