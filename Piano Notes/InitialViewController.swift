974d7d213dce6b37f7e0a8dd450f9f313a96889d
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-15 00:14:37 -0500

    add intro screen

---
+++ Piano Notes/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  Piano Notes
+//
+//  Created by Matvey on 4/14/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
