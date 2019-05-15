679f93a1fda44927d5f83ed9bc1454696314b316
Matvey Kostukovsky <matvelius@gmail.com>
2019-05-15 07:39:17 -0500

    add new notes on staff feature

---
+++ Piano Notes/NotesOnStaffViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesOnStaffViewController.swift
+//  Piano Notes
+//
+//  Created by Matvey on 5/15/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class NotesOnStaffViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
