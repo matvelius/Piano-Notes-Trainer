1aa33dbc8535004b51da697cacefb8d159d370c8
Matvey Kostukovsky <matvelius@gmail.com>
2019-05-25 14:57:24 +0300

    refactor currentNoteOnStaffImage code and add # button functionality

--- Piano Notes/NotesOnStaffViewController.swift
+++ Piano Notes/NotesOnStaffViewController.swift
@@ -12,10 +12,18 @@ class NotesOnStaffViewController: UIViewController {
     
     @IBOutlet weak var noteOnStaffImage: UIImageView!
     
+    var currentNoteOnStaffImageName = ""
+    
     @IBOutlet var panGestureRecognizerOutlet: UIPanGestureRecognizer!
     
     
     @IBAction func sharpButtonPressed(_ sender: UIButton) {
+//        str.insert("N", at: str.index(str.endIndex, offsetBy: -1))
+//        currentNoteOnStaffImageName = "staff\(whiteNotesOnLargeKeyboard[currentNoteIndex])"
+        if currentNoteOnStaffImageName[currentNoteOnStaffImageName.index(currentNoteOnStaffImageName.endIndex, offsetBy: -2)] != "#" {
+            currentNoteOnStaffImageName.insert("#", at: currentNoteOnStaffImageName.index(currentNoteOnStaffImageName.endIndex, offsetBy: -1))
+            updateNoteOnStaffImage(optionalImageName: currentNoteOnStaffImageName)
+        }
     }
     
     @IBAction func naturalButtonPressed(_ sender: UIButton) {
@@ -27,7 +35,7 @@ class NotesOnStaffViewController: UIViewController {
     @IBAction func upArrowButtonPressed(_ sender: UIButton) {
         if currentNoteIndex < whiteNotesOnLargeKeyboard.count - 1 {
             currentNoteIndex += 1
-            updateNoteOnStaffImage()
+            updateNoteOnStaffImage(optionalImageName: nil)
         }
     }
     
@@ -39,19 +47,18 @@ class NotesOnStaffViewController: UIViewController {
     @IBAction func downArrowButtonPressed(_ sender: UIButton) {
         if currentNoteIndex > 0 {
             currentNoteIndex -= 1
-            updateNoteOnStaffImage()
+            updateNoteOnStaffImage(optionalImageName: nil)
         }
     }
     
-    
-    
-    
+
     var currentNoteIndex = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        noteOnStaffImage.image = UIImage(named: "staff\(whiteNotesOnLargeKeyboard[currentNoteIndex])")
+        updateNoteOnStaffImage(optionalImageName: nil)
+        
     }
     
 //    var noteOnStaffPanGestureRecognizerLocation: CGFloat = 0
@@ -59,26 +66,28 @@ class NotesOnStaffViewController: UIViewController {
     var locationTracker: Double = Double(whiteNotesOnLargeKeyboard.count / 2) {
         didSet {
             currentNoteIndex = Int(round(locationTracker))
-            updateNoteOnStaffImage()
+            updateNoteOnStaffImage(optionalImageName: nil)
         }
     }
     
-    func updateNoteOnStaffImage() {
-//        while locationTracker > 0 && locationTracker < whiteNotesOnLargeKeyboard.count {
+    func updateNoteOnStaffImage(optionalImageName: String?) {
+        if let imageName = optionalImageName {
+            noteOnStaffImage.image = UIImage(named: imageName)
+        } else {
+            currentNoteOnStaffImageName = "staff\(whiteNotesOnLargeKeyboard[currentNoteIndex])"
+            noteOnStaffImage.image = UIImage(named: currentNoteOnStaffImageName)
+        }
         print("locationTracker: \(locationTracker)")
         print("currentNoteIndex: \(currentNoteIndex)")
-        noteOnStaffImage.image = UIImage(named: "staff\(whiteNotesOnLargeKeyboard[currentNoteIndex])")
-//        }
+
     }
     
     @IBAction func handlePanGesture(recognizer: UIPanGestureRecognizer) {
         
-//        noteOnStaffPanGestureRecognizerLocation = recognizer.location(in: self.noteOnStaffImage).y
-        
-        print(recognizer.velocity(in: self.view).y)
+//        print(recognizer.velocity(in: self.view).y)
         
         if recognizer.velocity(in: self.view).y < 0 && Int(locationTracker) < whiteNotesOnLargeKeyboard.count - 1 {
-            print("panning up!")
+//            print("panning up!")
             locationTracker += 0.17
             
             if recognizer.velocity(in: self.view).y < -150 && Int(locationTracker) < whiteNotesOnLargeKeyboard.count - 1 {
@@ -87,7 +96,7 @@ class NotesOnStaffViewController: UIViewController {
         
         } else if recognizer.velocity(in: self.view).y > 0 && locationTracker > 0 {
             locationTracker -= 0.17
-            print("panning down!")
+//            print("panning down!")
             
             if recognizer.velocity(in: self.view).y > 150 && locationTracker > 0 {
                 locationTracker -= 0.5
