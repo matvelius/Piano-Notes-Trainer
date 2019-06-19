92c2eadae28f9c28d3cdb8821ee4a2a3ab2919c3
Matvey Kostukovsky <matvelius@gmail.com>
2019-06-19 09:49:23 -0500

    add swiping controller (UICollectionViewController)

---
+++ Piano Notes/SwipingController.swift
@@ -0,0 +1,13 @@
+//
+//  SwipingController.swift
+//  Piano Notes
+//
+//  Created by Matvey on 6/19/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController {
+    
+}
